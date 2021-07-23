2904e3754400b8bacb23d9d2c955b3130cecb766
kevin-m <kmclaughlinbtb@gmail.com>
2021-07-22 21:35:50 -0500

    added user seed data and updated seed.js

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,12 @@
 const Restaurant = require('../models/restaurant')
 const Table = require('../models/table')
 const ReservationSlot = require('../models/reservationSlot')
+const User = require('../models/user')
 
 const seedData = require('./restaurant-seeds.json')
 const tableSeedData = require('./table-seeds.json')
 const reservationSlotSeedData = require('./reservationSlot-seeds.json')
+const userSeedData = require('./user-seeds.json')
 
 
 console.log(ReservationSlot);
@@ -58,6 +60,14 @@ Table.deleteMany({})
         process.exit()
     })
 
-    
-    
-    
+User.deleteMany({})
+    .then(() => {
+        return User.insertMany(userSeedData)
+    })
+    .then(console.log)
+    .catch((err) => {
+        console.log(err)
+    })
+    .finally(() => {
+        process.exit()
+    })
